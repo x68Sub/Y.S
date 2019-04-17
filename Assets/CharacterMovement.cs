e8c53a623a314fdf7fdbd3195c65f61e128421c6
x68Sub <49708842+x68Sub@users.noreply.github.com>
2019-04-17 15:56:14 +0900

    Add files via upload

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]private float j;
+    private float h;
+    private float v;
+    public float WalkSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxis("Vertical") * WalkSpeed;
+        h = Input.GetAxis("Horizontal") * WalkSpeed;
+        this.gameObject.transform.Translate(h, j, v);
+        if (Input.GetButtonDown("Jump"))
+        {
+            j = 1;
+            AboutJump();
+        }
+        if (Input.GetButtonUp("Jump")) {
+            j = 0;
+        }
+    }
+
+    void AboutJump()
+    {
+        float TIme = 5.0f;
+        TIme -= Time.deltaTime;
+        if ()
+    }
+}
---
+++ Assets/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField]private Vector3 offset;
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        offset = transform.position - Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Target.transform.position + offset;
+    }
+}
